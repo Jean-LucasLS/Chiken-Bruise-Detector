0a56675665d59becc19f524eb9d038e83f8c4bae
Jean-Lucas <jean.lucasls96@gmail.com>
2024-08-05 18:51:46 -0300

    cfc

--- bruiser_detection.py
+++ bruiser_detection.py
@@ -1,6 +1,17 @@
 import cv2
 import numpy as np
 
+# Define the frame interest area
+def cut_frame(frame):
+  height, width = frame.shape[:2]
+  fr_x_start    = int(width * 0.25)
+  fr_x_end      = int(width * 0.75)
+  fr_y_start    = int(height * 0.20)
+  fr_y_end      = int(height * 0.80)
+
+  frame = frame[fr_y_start:fr_y_end, fr_x_start:fr_x_end]
+  return frame
+
 def rectangle_filler(frame, contours):
   for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
@@ -10,18 +21,24 @@ def rectangle_filler(frame, contours):
 def bruiser_detector(frame):
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Convert the frame to HSV color space
 
-  # Define range for the color that could represent a bruise (e.g., shades of blue/purple)
-  lower_red = np.array([0, 50, 50])
-  upper_red = np.array([10, 255, 255])
+  frame = cut_frame(frame)
 
-  # Create a mask with the specified color range
-  mask = cv2.inRange(hsv, lower_red, upper_red)
+  # Define range for the color that could represent a bruise
+  lower_color1 = np.array([0, 50, 50])
+  upper_color1 = np.array([10, 255, 255])
+  lower_color2 = np.array([160, 50, 50])
+  upper_color2 = np.array([180, 255, 255])
   
-  kernel = np.ones((100, 100), np.uint8)
-  mask = cv2.dilate(mask, kernel, iterations=1)
+  # Create masks for the lower and upper color ranges
+  mask1 = cv2.inRange(hsv, lower_color1, upper_color1)
+  mask2 = cv2.inRange(hsv, lower_color2, upper_color2)
+  mask  = cv2.bitwise_or(mask1, mask2)
 
-  # Find contours in the mask
-  contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  # Dilation to increase the size of the red areas
+  kernel = np.ones((50, 50), np.uint8)
+  mask   = cv2.dilate(mask, kernel, iterations=1)
+  
+  contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Find contours in the mask
 
   frame = rectangle_filler(frame, contours)
   return frame
--- frame_capture.py
+++ frame_capture.py
@@ -1,36 +1,40 @@
 import cv2
-from bruiser_detection import bruiser_detector, rectangle_filler
 
-VIDEO_PATH   = 'videos/20240216_094022.mp4'
+from bruiser_detection import bruiser_detector
+
+VIDEO_PATH   = 'videos/Frango1.mp4'
+FRAME_CAP    = 1 # Set the seconds between frame captures
 
 cap = cv2.VideoCapture(VIDEO_PATH)
 if not cap.isOpened(): exit('Error')
 
-fps_rate    = cap.get(cv2.CAP_PROP_FPS) # Obtenha a taxa de frames por segundo do vídeo
-WAIT_TIME   = int(1000 / fps_rate) # Calculate the wait time to play the video in real-time (1x)
-FRAME_CAP   = 6 # Set the seconds between frame captures
+fps_rate    = cap.get(cv2.CAP_PROP_FPS) # Get the frame rate per second
 interval    = int(fps_rate * FRAME_CAP) # Calculate the interval in frames
 frame_index = 0
 
+WAIT_TIME   = int(1000 / fps_rate) # Calculate the wait time to play the video in real-time (1x)
+
 while cap.isOpened():
   ret, frame = cap.read()
-
-  # cv2.imshow('Video', frame)
 
   if ret:
     if frame_index % interval == 0:
 
       frame = bruiser_detector(frame)
+
       frame_name = f'frames/frame_{int(frame_index/fps_rate):04d}.jpg'
       cv2.imwrite(frame_name, frame)
       print(f'Saving {frame_name}')
 
     frame_index += 1
 
-    # if cv2.waitKey(WAIT_TIME) & 0xFF == ord('q'):
-    #   break
+    if cv2.waitKey(WAIT_TIME) & 0xFF == ord('q'): # Press 'q' to stop the video
+      break
+
   else:
     break
+
+  cv2.imshow('Video', frame)
 
 cap.release()
 cv2.destroyAllWindows()
